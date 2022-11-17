fa5ee168ae385d5db323ce0249b53ca9f20cbdda
geetanjali chawla <geetc1919@gmail.com>
2022-11-17 18:11:31 +0530

    git push origin main

---
+++ src/components/about-breif/about.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import './about.css'
+export default function About() {
+  return (
+  <div className="about-breif">
+ <Container>
+     <div className="about-content p-4 m-2">
+      <h2>
+      From strategy to delivery, we are here <span className="brown-color">to make sure your business</span> endeavor succeeds. Trust our experts.
+      </h2>
+      <h6 className="about-name">
+      ALEX LEE, CEO
+      </h6>
+    </div>
+    </Container>
+  </div>
+  )
+}
---
+++ src/components/nav/nav.jsx
@@ -0,0 +1,35 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import logo from "../../logo.jpg";
+import './nav.css'
+
+
+function MyNav() {
+  return (
+    <Navbar className='bg-trasparent navbar-m' collapseOnSelect expand="lg"  variant="light" sticky="top" >
+      <Container>
+        <Navbar.Brand href="#" class="ms-0">
+            <img className="logo-img " src={logo} alt="logo img" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mx-auto">
+          <Nav.Link className="hover-before nav-link-font active" href="#">HOME</Nav.Link>
+          <Nav.Link className="hover-before nav-link-font" href="#">ABOUT US</Nav.Link>
+          <Nav.Link className="hover-before nav-link-font" href="#">SERVICES</Nav.Link>
+          <Nav.Link className="hover-before nav-link-font" href="#">BRANDS</Nav.Link>
+          <Nav.Link className="hover-before nav-link-font" href="#">CONTACT US</Nav.Link>
+          <Nav.Link className="hover-before nav-link-font" href="#">BLOG</Nav.Link>
+          </Nav>
+          {/* <Nav>
+            <Nav.Link className='nav-link-font' href="#"  onClick={()=>{
+props.setIntro(true)}}>info</Nav.Link>
+          </Nav> */}
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default MyNav;
