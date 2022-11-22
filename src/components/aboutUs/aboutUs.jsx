c5765875e3c38ee41317f10d7fcaf39d542e6cd1
geetanjali chawla <geetc1919@gmail.com>
2022-11-22 15:26:14 +0530

    about brief componet added

--- src/components/aboutUs/aboutUs.jsx
+++ src/components/aboutUs/aboutUs.jsx
@@ -1,5 +1,6 @@
 
 import React from 'react';
+import About from '../body/about-breif/about';
 // import { Container } from 'react-bootstrap';
 import Contnent from './componets/contnent';
 import Title from './componets/title';
@@ -9,6 +10,12 @@ const AboutUs = () => {
     return (
         <div>
           <Title/>
+          <About
+          title1 = {"Launch your creative ideas. Whatever you "}
+          title2 ={"imagine we"}
+          title3 = {"will make it happen with our team of specialists."}
+          ceo = {"ALEX LEE, CEO"}/>
+         
           <Contnent/>
 
         </div>
--- src/components/home page/home.jsx
+++ src/components/home page/home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Carousel from './componets/banner/Carousel';
-import About from './componets/about-breif/about'
+import About from '../body/about-breif/about'
 import Features from './componets/features/features'
 import BusinessServies from './componets/businessServies/businessServies';
 import Applynow from './componets/applynow/applynow';
